517827646092c4553191e83efd98ab980ef380ce
Jesus Armando Hernandez Payan <jesus.he.payan@gmail.com>
2022-03-10 09:43:09 -0700

    Adding the source files

---
+++ src/test/java/com/SpringJavaFXIntegration/SpringJavaFXIntegration/SpringJavaFxIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringJavaFXIntegration.SpringJavaFXIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaFxIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
